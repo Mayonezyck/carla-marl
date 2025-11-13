4ebb987025f86c2406e321508ab47e56e2e30739
yicheng zhu <yz8733@rit.edu>
2025-11-13 14:09:21 -0500

    add client and pipeline file

---
+++ client.py
@@ -0,0 +1,4 @@
+class carlaClient:
+    def __init__(self):
+        print('carla client initiated')
+        pass
---
+++ pipeline.py
@@ -0,0 +1,11 @@
+# This code is the one and only pipeline that handles different modules, wire them together, and 
+# import 
+import carla
+import yaml
+from client import carlaClient
+
+if __name__ == "__main__": 
+    #Starting Carla Client
+    client = carlaClient()
+
+    pass
